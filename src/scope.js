1c4be23d05dd8ea1b7ede4fc62bf738d35ae9a1b
VladShaluhin <zair1985@gmail.com>
2015-01-21 01:23:25 +0200

    Watching Several Changes With One Listener:
    Added the basic behavior of ,

--- src/scope.js
+++ src/scope.js
@@ -107,6 +107,29 @@ Scope.prototype.$applyAsync = function(expr) {
   }
 };
 
+Scope.prototype.$watchGroup = function(watchFns, listenerFn) {
+  var self = this;
+  var oldValues = new Array(watchFns.length);
+  var newValues = new Array(watchFns.length);
+  var changeReactionScheduled = false;
+
+  function watchGroupListener() {
+    listenerFn(newValues, oldValues, self);
+    changeReactionScheduled = false;
+  }
+
+  _.forEach(watchFns, function(watchFn, i) {
+    self.$watch(watchFn, function(newValue, oldValue) {
+      newValues[i] = newValue;
+      oldValues[i] = oldValue;
+      if (!changeReactionScheduled) {
+        changeReactionScheduled = true;
+        self.$evalAsync(watchGroupListener);
+      }
+    });
+  });
+};
+
 Scope.prototype.$beginPhase = function(phase) {
   if(this.$$phase) {
     throw this.$$phase + ' already in progress';
